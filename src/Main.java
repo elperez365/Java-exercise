a151fe8743d851094164196dc406692e570b588c
elperez365 <ceti.vasco@gmail.com>
2023-09-01 09:10:59 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Persona persona1 = new Persona();
+        Persona persona1 = new Persona("luca", "rossi", 30, "blu");
+        // persona1 adesso è un oggetto di tipo persona
+
+        System.out.println(persona1.colorePreferito);
+
+        persona1.cammina();
+
+        Persona persona2 = new Persona("Anna", "verdi", 51, "verde");
+
+        System.out.println(persona2.eta);
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,8 +1,16 @@
 public class Persona {
-    String nome = "Luca";
-    String cognome = "Rossi";
-    int eta = 25;
-    String colorePreferito = "blu";
+    String nome;
+    String cognome;
+    int eta;
+    String colorePreferito;
+
+    Persona(String nome, String cognome, int eta, String colorePreferito) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+        this.colorePreferito = colorePreferito;
+
+    }
 
     void saluta() {
         System.out.println("ciao sono" + nome);
